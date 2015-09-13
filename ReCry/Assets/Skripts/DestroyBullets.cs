cb7f888cba0ba2716e26c8ef2721fef849251f6e
Scr4py <kevinholst@gmx.net>
2015-09-13 17:01:57 +0200

    [CHANGED] Playerprefab has now a Muzzle [NEW] Bulletprefab, ShootController [DELETE]CameraController Skript

---
+++ ReCry/Assets/Skripts/DestroyBullets.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBullets : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        Invoke("Destroy", 5f);
+    }
+
+    void Destroy()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
---
+++ ReCry/Assets/Skripts/ShootController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ShootController : MonoBehaviour {
+
+    public GameObject AssaultBullet;
+    public float BulletSpeed = 30;
+    private List<GameObject> AssaultBulletPool;
+    private int pooledAmount = 30;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.AssaultBulletPool = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            GameObject bullet = Instantiate(AssaultBullet) as GameObject;
+            bullet.SetActive(false);
+            AssaultBulletPool.Add(bullet);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Fire();
+	}
+
+    void Fire()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            foreach (var bullet in AssaultBulletPool)
+            {
+                if (!bullet.activeInHierarchy)
+                {
+                    bullet.SetActive(true);
+                    bullet.transform.position = this.transform.position;
+                    bullet.GetComponent<Rigidbody>().AddForce(this.transform.forward * BulletSpeed, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
