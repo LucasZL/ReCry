2f9f1bd04521f18339172d9bfdad2cca713baf80
Lucas Zacharias-Langhans <Lucas@Lucass-MacBook-Pro.local>
2015-09-21 17:07:46 +0200

    [CHANGED]Github stuff

---
+++ ReCry/Assets/Skripts/RayCastShoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastShoot : MonoBehaviour {
+
+    float maxRange = 80;
+    RaycastHit hit;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void RayCastFire()
+    {
+        Ray RayCast = new Ray(this.transform.position, Vector3.forward);
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(RayCast, out hit, maxRange))
+            {
+                if (hit.collider.tag == "Player")
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ ReCry/Assets/Skripts/Utility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Photon;
+public static class Utility
+{
+    
+    public static string ServerName;
+    public static RoomOptions roomOptions;
+    public static RoomInfo[] roomInfo;
+}
