dd28d45cdf0ad81df235ae444aa892440ea8e50f
Kevin Holst <kevinholst@gmx.net>
2016-01-13 18:12:20 +0100

    Teamselection now works

--- ReCry/Assets/Skripts/UI/TeamSelection.cs
+++ ReCry/Assets/Skripts/UI/TeamSelection.cs
@@ -3,105 +3,70 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine.UI;
 
-public class TeamSelection : Photon.MonoBehaviour {
-
-    List<PhotonPlayer> blueteam;
-    List<PhotonPlayer> redteam;
-    List<PhotonPlayer> cyanteam;
-    List<PhotonPlayer> greenteam;
-    
+public class TeamSelection : Photon.MonoBehaviour
+{
     public Text BlueTeam;
     public Text RedTeam;
     public Text CyanTeam;
     public Text GreenTeam;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        if (PhotonNetwork.isMasterClient)
-        {
-            blueteam = new List<PhotonPlayer>();
-            redteam = new List<PhotonPlayer>();
-            cyanteam = new List<PhotonPlayer>();
-            greenteam = new List<PhotonPlayer>();
-            Invoke("SendTeams", 1);
-        }
-        photonView.RPC("CheckStats", PhotonTargets.OthersBuffered);
-    }
-    void SendTeams()
-    {
-        photonView.RPC("SendTeamList", PhotonTargets.OthersBuffered, blueteam, cyanteam, redteam, greenteam);
-    }
-    [PunRPC]
-    void SendTeamLists(List<PhotonPlayer> blue, List<PhotonPlayer> cyan, List<PhotonPlayer> red, List<PhotonPlayer> green)
-    {
-        blueteam = blue;
-        cyanteam = cyan;
-        redteam = red;
-        greenteam = green;
     }
 
-    [PunRPC]
+    void Update()
+    {
+        foreach (var team in PunTeams.PlayersPerTeam)
+        {
+            switch (team.Key)
+            {
+                case PunTeams.Team.none:
+                    break;
+                case PunTeams.Team.red:
+                    RedTeam.text = team.Value.Count.ToString();
+                    break;
+                case PunTeams.Team.darkblue:
+                    BlueTeam.text = team.Value.Count.ToString();
+                    break;
+                case PunTeams.Team.cyan:
+                    CyanTeam.text = team.Value.Count.ToString();
+                    break;
+                case PunTeams.Team.green:
+                    GreenTeam.text = team.Value.Count.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    
     public void JoinCyanTeam()
     {
-        if (cyanteam.Count >= 8)
-        {
-            
-        }
-        else
-        {
-            cyanteam.Add(PhotonNetwork.player);
-        }
+        PhotonNetwork.player.SetTeam(PunTeams.Team.cyan);
     }
-    [PunRPC]
+
     public void JoinBlueTeam()
     {
-        if (blueteam.Count >= 8)
-        {
+        PhotonNetwork.player.SetTeam(PunTeams.Team.darkblue);
 
-        }
-        else
-        {
-            blueteam.Add(PhotonNetwork.player);
-        }
     }
-    [PunRPC]
+
     public void JoinRedTeam()
     {
-        if (redteam.Count >= 8)
-        {
+        PhotonNetwork.player.SetTeam(PunTeams.Team.red);
 
-        }
-        else
-        {
-            redteam.Add(PhotonNetwork.player);
-        }
     }
-    [PunRPC]
+
     public void JoinGreenTeam()
     {
-        if (greenteam.Count >= 8)
-        {
-
-        }
-        else
-        {
-            greenteam.Add(PhotonNetwork.player);
-        }
-    }
-
-
-    [PunRPC]
-    void CheckStats()
-    {
-        BlueTeam.text = string.Format("{0} / 8", blueteam.Count);
-        RedTeam.text = string.Format("{0} / 8", redteam.Count);
-        CyanTeam.text = string.Format("{0} / 8", cyanteam.Count);
-        GreenTeam.text = string.Format("{0} / 8", greenteam.Count);
+        PhotonNetwork.player.SetTeam(PunTeams.Team.green);
     }
 
     public virtual void OnPhotonSerializeView()
     {
 
     }
+
+
 }
