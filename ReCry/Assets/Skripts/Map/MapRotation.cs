d2a1ed1c38eea2ec75f140965fc1ca98eddeefa3
Kevin Holst <kevinholst@gmx.net>
2016-01-18 17:39:27 +0100

    [Fixed] MapRotation

---
+++ ReCry/Assets/Skripts/Map/MapRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapRotation : MonoBehaviour
+{
+    public float speed = 10;
+    public GameObject parent;
+
+    void Start()
+    {
+        parent = GameObject.Find("PivotPoint");
+    }
+
+    void Update()
+    {
+        this.transform.RotateAround(parent.transform.position, new Vector3(0, speed, 0), Time.deltaTime * speed);
+    }
+}
+
