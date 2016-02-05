81b982781d57e32a695fee3336e19544cf29a39c
Kevin Holst <kevinholst@gmx.net>
2016-02-05 14:45:15 +0100

    [Changed] menu Background, Intro Scene

---
+++ ReCry/Assets/Skripts/UI/Intro.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    public GameObject SAEImage;
+    void Start()
+    {
+        StartCoroutine(ChangeImage());
+    }
+
+    IEnumerator ChangeImage()
+    {
+        SAEImage.SetActive(true);
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene("Menu");
+    }
+}
